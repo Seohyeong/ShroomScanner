3f8ac6bd7b652b402c2f7ffcb681a2a3ca78763a
Seohyeong <jseoh95@gmail.com>
2024-08-26 13:42:37 +1000

    Initial Commit

---
+++ ShroomScanner/ShroomScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShroomScannerApp.swift
+//  ShroomScanner
+//
+//  Created by Seohyeong Jeong on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ShroomScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
